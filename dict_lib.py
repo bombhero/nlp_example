3bec4b543bea8316c697630e157d7700b0bf3d22
bomb <bomb.zhang@gmail.com>
2021-01-07 21:18:51 +0800

    Temp version

--- dict_lib.py
+++ dict_lib.py
@@ -4,9 +4,11 @@ import jieba
 
 
 dict_path = 'c:/tmp/dict.csv'
+line_limit = 0
 
 
 def analyze_corpus(file_path, dict_df):
+    line_count = 0
     fid = open(file_path, 'rb')
     while True:
         line = fid.readline()
@@ -14,16 +16,20 @@ def analyze_corpus(file_path, dict_df):
             break
         if len(line.strip()) == 0:
             continue
+        line_count += 1
+        if (line_count >= line_limit) and (line_limit > 0):
+            break
         word_list = jieba.lcut(line)
         for word in word_list:
             if len(word.strip()) == 0:
                 continue
             if len(dict_df[dict_df['word'] == word]) == 0:
                 dict_df = dict_df.append({'word': word, 'count': 1}, ignore_index=True)
-                print('Add {}, dict size: {}'.format(word, len(dict_df)))
+                print('Add {}, dict size: {}, total: {}'.format(word, len(dict_df), line_count))
             else:
                 dict_df.loc[dict_df['word'].isin([word]), 'count'] += 1
     fid.close()
+    return dict_df
 
 
 def create_dict_cn(corpus_path, max_word=10000):
@@ -35,13 +41,16 @@ def create_dict_cn(corpus_path, max_word=10000):
     :return:
     """
     dict_df = pd.DataFrame(columns=('word', 'count'))
-    file_list = os.listdir(corpus_path)
-    for file_name in file_list:
-        file_path = corpus_path + '/' + file_name
-        analyze_corpus(file_path, dict_df)
+    if os.path.isdir(corpus_path):
+        file_list = os.listdir(corpus_path)
+        for file_name in file_list:
+            file_path = corpus_path + '/' + file_name
+            analyze_corpus(file_path, dict_df)
+    else:
+        dict_df = analyze_corpus(corpus_path, dict_df)
     return dict_df
 
 
 if __name__ == '__main__':
-    df = create_dict_cn(corpus_path='C:/bomb/proj/corpus')
+    df = create_dict_cn(corpus_path='C:/bomb/book/ML/NLP/zenghuanzuang.txt')
     df.to_csv(path_or_buf=dict_path, sep=',')
